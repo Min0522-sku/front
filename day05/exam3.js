4c29c3072853203d7c4f41d801ed958f2ff73719
Min0522-sku <wkdalstj0522@gmail.com>
2025-12-30 18:15:38 +0900

    오후

---
+++ day05/exam3.js
@@ -0,0 +1,56 @@
+// let num1 = parseInt(prompt("점수1 :"))
+// let num2 = parseInt(prompt("점수2 :"))
+// if (num1+num2 >= 90) {console.log("성공")}
+// else {console.log("실패")}
+
+// let n1 = parseInt(prompt("정수1 : "))
+// let n2 = parseInt(prompt("정수2 : "))
+// if(n1 > n2) {console.log(`정수1 : ${n1}가 더 큽니다`)}
+// else {console.log(`정수2 : ${n2}가 더 큽니다`)}
+
+// let id = prompt("id : ")
+// let password = prompt("password : ")
+// if (id == 'admin' && password == '1234') { console.log("로그인 성공")}
+// else {console.log("로그인 실패")}
+
+// let password = prompt("password : ")
+// if(password.length < 8) {console.log("보안등급: 약함 (8자 이상으로 설정해주세요.")}
+// else if(password.length >= 8 && password.length < 12) {console.log("보안등급 : 보통")}
+// else if(password.length >= 12) {console.log("보안 등급: 강함")}
+
+// let seatList = ['O', 'X', 'O']
+// let n = parseInt(prompt("좌석 번호 : "))
+// if (seatList[n] == 'O') {console.log("예약성공")}
+// else if (seatList[n] == 'X') {console.log("예약 불가")} 
+
+// let s = parseInt(prompt("점수 :"))
+// if (s >= 900) {console.log("A급 경품")}
+// else if (s >= 700) {console.log("B급 경품")}
+// else if (s >= 500) {console.log("C급 경품")}
+// else {console.log("참가상")}
+
+// let role = prompt("역할 : ")
+// if (role == 'admin') {console.log("admin: '모든기능에 접근할 수 있습니다.'")}
+// else if (role == 'editor') {console.log("editor: '콘텐츠 수정 및 생성 기능에 접근할 수 있습니다.'")}
+// else if (role == 'viewer') {console.log("viewer: '콘텐츠 조회만 가능합니다.'")}
+// else {console.log("그외 역할: '정의되지 않은 역할입니다.")}
+
+// let age = parseInt(prompt("나이 : "))
+// if(age >= 65) {console.log("65세 이상 : '3,000원'")}
+// else if(age >= 20) {console.log("20세 이상 65세 미만: '10,000원'")}
+// else if(age >=8) {console.log("8세 이상 19세 이하 : '5,000원'")}
+// else {console.log("8세 미만: '무료'")}
+
+// let n = Number(prompt("점수 : "))
+// if (n >= 90) {console.log("90점 이상: 'A등급'")}
+// else if (n >= 80) {console.log("80점 이상 90점 미만: 'B등급'")}
+// else if (n >= 70) {console.log("70점 이상 80점 미만: 'C등급'")}
+// else {console.log("70점 미만: '재시험'")}
+
+// let drinkNames = ['콜라', '사이다', '커피']
+// let drinkPrices = [1000, 1000, 1500]
+// let num = parseInt(prompt("원하는 음료 번호 : "))
+// if(num == 0 || num == 1 || num == 2) {console.log(`선택하신 음료는 [${drinkNames[num]}]입니다. 가격은 [${drinkPrices[num]}]원입니다.`)}
+// else {console.log("없는 상품입니다")}
+
+
---
+++ day05/js_practice4.js
@@ -0,0 +1,48 @@
+// let fruitList = ['사과', '바나나']
+// let inputFruit = prompt("새로운 과일 : ")
+// if (fruitList.includes(inputFruit)) {console.log("이미 존재하는 과일입니다.")}
+// else {
+//     fruitList.push(inputFruit)
+//     console.log(fruitList)
+// }
+
+// let jumin = prompt("주민번호 : ")
+// if(jumin[6] == 1 || jumin[6] == 3) {console.log("blue")}
+// else if(jumin[6] == 2 || jumin[6] == 4) {console.log("red")}
+
+// let buy = Number(prompt("총 구매 금액 : "))
+// if(buy >= 50000) {buy -= (buy*0.1)}
+// else if(buy >= 30000) {buy -= (buy*0.05)}
+// else if(buy >= 10000) {buy -= (buy*0.01)}
+// console.log(`최종 결제 금액 : ${buy}원`)
+
+// let month = parseInt(prompt("월을 숫자로 입력 : "))
+// if(month == 1 || month == 2 || month == 12) {console.log("겨울")}
+// else if(month == 3 || month == 4 || month == 5) {console.log("봄")}
+// else if(month == 6 || month == 7 || month == 8) {console.log("여름")}
+// else if(month == 9 || month == 10 || month == 11) {console.log("가을")}
+// else {console.log("잘못된 월입니다.")}
+
+// let n1 = parseInt(prompt("정수1 : "))
+// let n2 = parseInt(prompt("정수2 : "))
+// let n3 = parseInt(prompt("정수3 : "))
+// console.log(`가장 큰수 : ${Math.max(n1, n2, n3)}`)
+
+// let year = parseInt(prompt("연도를 입력하세요 : "))
+// if((year%4 == 0 && year%100 != 0) || year % 400 == 0) { console.log(`${year}은 윤년입니다.`)}
+// else {console.log(`${year}은 평년입니다.`)}
+
+// let n1 = parseInt(prompt("정수1 :"))
+// let n2 = parseInt(prompt("정수2 :"))
+// let n3 = parseInt(prompt("정수3 :")) 이거 경우의 수가 너무 많은데요 선생님?
+
+// let p1 = prompt("p1 : ")
+// let p2 = prompt("p2 : ")
+// if(p1 == p2) {console.log("무승부")}
+// else if(p1 == 0 && p2 == 2) {console.log("p1 승리")}
+// else if(p1 == 0 && p2 == 1) {console.log("p2 승리")}
+// else if(p1 == 1 && p2 == 2) {console.log("p2 승리")}
+// else if(p1 == 2 && p2 == 1) {console.log("p1 승리")}
+// else if(p1 == 1 && p2 == 0) {console.log("p1 승리")}
+// else if(p1 == 2 && p2 == 0) {console.log("p2 승리")}
+
