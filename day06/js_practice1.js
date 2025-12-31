5b29929637015fff92dcc2d17d8bf18e8426d2ae
Min0522-sku <wkdalstj0522@gmail.com>
2025-12-31 14:54:37 +0900

    반복문

---
+++ day06/js_practice1.js
@@ -0,0 +1,23 @@
+for(let i = 1; i <= 10; i++){
+    console.log(i)
+}
+
+let fruits = ['사과', '바나나', '포도', '딸기']
+for(let j = 0; j <= fruits.length-1; j++){
+    console.log(fruits[j])
+}
+
+let numbers  = [1,2,3,4,5]
+for(let k = numbers.length-1; k >= 0; k--){
+    console.log(numbers[k])
+}
+
+let result = 0
+for(let n = 1; n <= 50; n++){
+    result += n
+}
+console.log(result)
+
+for(let l = 1; l <= 20; l++){
+    if(l%2 == 0) {console.log(l) }
+}
